67c3d1feb9b87dda89523db1f702790431942fa5
Pascal Martineau <pascal@lewebsimple.ca>
2017-08-16 13:36:18 -0400

    Dynamically show/hide select inputs

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,7 +1,36 @@
 (function ($) {
 
   function initialize_field ($el) {
-    console.log('TODO: Initialize ACF Tree Select field');
+    $el.find('select').each(function () {
+
+      // Store previous value
+      $(this).data('previous', this.value);
+
+      // Monitor for selection changes
+      $(this).on('change', function (e) {
+        let index = this.name.lastIndexOf('[value]');
+
+        // Hide and reset value previous selection's child input
+        let toHide = this.name.substr(0, index) + '[' + $(this).data('previous') + ']' + this.name.substr(index);
+        $el.find('select[name="' + toHide + '"]').each(function () {
+          $(this).val('');
+          $(this).hide();
+        });
+
+        // Show latest selection
+        if (this.value !== '') {
+          let toShow = this.name.substr(0, index) + '[' + this.value + ']' + this.name.substr(index);
+          $el.find('select[name="' + toShow + '"]').each(function () {
+            $(this).show();
+          });
+        }
+
+        // Update previous value
+        $(this).data('previous', this.value);
+
+      });
+
+    });
   }
 
   if (typeof acf.add_action !== 'undefined') {
